d0ca3b332f2f5d1c934dc4e15a492a51bfb8c47a
Mike Christle <info@christle.us>
2020-09-07 09:32:10 -0400

    Fir paths in build batch files

--- Miny18/init.bat
+++ Miny18/init.bat
@@ -1,5 +1,5 @@
 set JAVA_BIN="C:\Program Files\Java\jdk-12.0.1\bin"
-set MINY_BIN="C:\Users\mike\ProjectsSW\MiniRover\Mini"
+set MINY_BIN="C:\Users\mike\Projects\Proc18Miny18\Miny18"
 set CPATH=..\class
 set MINY_UTIL=..\util\
 PATH=%JAVA_BIN%;%MINY_BIN%;%PATH%
--- Miny18/minysim.bat
+++ Miny18/minysim.bat
@@ -1,5 +1,5 @@
 echo off
-set DEST=C:\Users\mike\Projects\Proc18Miny18\Proc18\proc
+set DEST=C:\Users\mike\Projects\Proc18Miny18\Proc18\sim
 cls
 java -cp %CPATH% Miny18 -a %1 %2 %3 %4 %5
 IF %ERRORLEVEL% NEQ 0  goto exit
