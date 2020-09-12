0146f55b1bbd88e12b6b202b0e33111052926629
Mike Christle <info@christle.us>
2020-09-12 12:10:15 -0400

    Delete shift register Correct bit size on state numbers

--- Proc18/util/SonyIR.v
+++ Proc18/util/SonyIR.v
@@ -10,6 +10,8 @@
 // 0.1.0   07/14/2019   File Created
 // 1.0.0   09/01/2020   Initial release
 // 1.1.0   09/01/2020   Add parameter to set bit count
+// 1.1.1   09/12/2020   Delete shift register
+//                      Correct bit size on state numbers
 //////////////////////////////////////////////////////////////////////////////
 // Copyright 2020 Mike Christle
 //
@@ -48,7 +50,6 @@ module SonyIR(CLK, CLK_10U, RESET, CS, IR, READY, DO);
     output reg READY = 0;
     output reg [BIT_CNT - 1:0] DO = 0;
 
-    reg [BIT_CNT - 1:0] shift;
     reg [2:0] state = 0;
     reg ir0, ir1, active_edge;
     reg [15:0] time_out = 0;
@@ -60,7 +61,6 @@ module SonyIR(CLK, CLK_10U, RESET, CS, IR, READY, DO);
             READY <= 0;
             state <= 0;
             active_edge <= 0;
-            shift <= 0;
             bit_cnt <= 0;
             ir0 <= 0;
             ir1 <= 0;
@@ -82,13 +82,13 @@ module SonyIR(CLK, CLK_10U, RESET, CS, IR, READY, DO);
             casex ({active_edge, state})
 
             // State 0, wait for start pulse
-            6'b1_000: begin
+            4'b1_000: begin
                 state <= 1;
                 time_out <= 250;
                 end
               
             // State 1, verify start pulse
-            6'b0_001: begin
+            4'b0_001: begin
                 time_out <= time_out - 1;
                 if (time_out == 0) begin
                     bit_cnt <= BIT_CNT - 1;
@@ -97,27 +97,27 @@ module SonyIR(CLK, CLK_10U, RESET, CS, IR, READY, DO);
                     end
                 end
 
-            6'b1_001: begin
+            4'b1_001: begin
                 state <= 6;
                 end
 
             // State 2, wait for active edge
-            6'b1_010: begin
+            4'b1_010: begin
                 state <= 3;
                 time_out <= 90;
                 end
 
-            6'b0_010: begin
+            4'b0_010: begin
                 time_out <= time_out - 1;
                 if (time_out == 0) state <= 30;
                 end
 
 
             // State 3, start data bit loop
-            6'b0_011: begin
+            4'b0_011: begin
                 time_out <= time_out - 1;
                 if (time_out == 0) begin
-                    shift <= {~ir1, shift[BIT_CNT - 1:1]};
+                    DO <= {~ir1, DO[BIT_CNT - 1:1]};
                     bit_cnt <= bit_cnt - 1;
                     time_out <= 120;
                     if (bit_cnt == 0) state <= 4;
@@ -125,25 +125,24 @@ module SonyIR(CLK, CLK_10U, RESET, CS, IR, READY, DO);
                     end
                 end
 
-            6'b1_011: begin
+            4'b1_011: begin
                 state <= 6;
                 end
 
             // State 4, save valid data
-            6'bX_100: begin
+            4'bX_100: begin
                 state <= 6;
-                DO <= shift;
                 READY <= 1;
                 end
 
             // State 6, ignore input for 10mSec
-            6'bX_110: begin
+            4'bX_110: begin
                 state <= 7;
                 time_out <= 3000;
                 end
 
             // State 7, ignore input for 10mSec
-            6'bX_111: begin
+            4'bX_111: begin
                 if (time_out == 0) state <= 0;
                 else time_out <= time_out - 1;
                 end
