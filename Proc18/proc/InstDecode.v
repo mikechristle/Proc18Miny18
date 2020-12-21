e4c70d916d81c2f7d241842502af4d73b844c307
Mike Christle <info@christle.us>
2020-12-21 10:57:16 -0500

    Issue 1 - Interrupts cause ALU ops to execute twice.
    When an interrupt occurs on an ALU op the ALU op executes, then when the
    ISR returns, the ALU op gets executed again.

--- Proc18/proc/InstDecode.v
+++ Proc18/proc/InstDecode.v
@@ -11,6 +11,7 @@
 // 1.1.0   09/07/2020   Add one clock delay on RUN input to sync to CLK
 // 1.2.0   09/08/2020   Optimize interrupt processing and cleanup
 // 1.2.1   09/13/2020   Specify bit size on addition operations
+// 1.3.0   12/19/2020   Issue 1, interrupts cause ALU ops to execute twice
 //////////////////////////////////////////////////////////////////////////////
 // Copyright 2020 Mike Christle
 //
@@ -90,154 +91,164 @@ module InstDecode(
                STATE_8 = 10'b0100000000, // 0400 
                STATE_9 = 10'b1000000000; // 1000 Interrupt
 
+    localparam ZERO = 1'b0, ONE = 1'b1;
+
     reg [9:0] state = 0;
     reg [5:0] dadrs_temp = 0;
     reg [3:0] level = 15;
     reg stat_flag = 0;
 
     wire [11:0] pc_plus_one = PC + 12'd1;
-    wire interrupt = (VECTOR > level) && (state[0] || state[4]);
+    wire interrupt = (VECTOR > level) & (state[0] | state[4]);
 
-    wire op_halt  = INST[17:6] == 12'o0001;
-    wire op_pause = INST[17:6] == 12'o0002;
-    wire op_rts   = INST[17:6] == 12'o0003;
-    wire op_rti   = INST[17:6] == 12'o0004;
-    wire op_level = INST[17:6] == 12'o0005;
-    wire op_reset = INST[17:6] == 12'o0006;
-    wire op_ldi   = INST[17:6] == 12'o0007;
+    wire op_halt  = state[0] & (INST[17:6] == 12'o0001);
+    wire op_pause = state[0] & (INST[17:6] == 12'o0002);
+    wire op_rts   = state[0] & (INST[17:6] == 12'o0003);
+    wire op_rti   = state[0] & (INST[17:6] == 12'o0004);
+    wire op_level = state[0] & (INST[17:6] == 12'o0005);
+    wire op_reset = state[0] & (INST[17:6] == 12'o0006);
+    wire op_ldi   = state[0] & (INST[17:6] == 12'o0007);
 
-    wire op_call  = INST[17:12] == 6'o01;
-    wire op_ldr   = INST[17:12] == 6'o02;
-    wire op_str   = INST[17:12] == 6'o03;
-    wire op_ldc   = INST[17:12] == 6'o04;
-    wire op_timer = INST[17:12] == 6'o05;
-    wire op_in    = INST[17:12] == 6'o06;
-    wire op_out   = INST[17:12] == 6'o07;
-    wire op_sxx   = INST[17:15] == 3'o1;
-    wire op_jxx   = INST[17:15] == 3'o2;
-    wire op_alub  = INST[17:15] == 3'o3;
+    wire op_call  = state[0] & (INST[17:12] == 6'o01);
+    wire op_ldr   = state[0] & (INST[17:12] == 6'o02);
+    wire op_str   = state[0] & (INST[17:12] == 6'o03);
+    wire op_ldc   = state[0] & (INST[17:12] == 6'o04);
+    wire op_timer = state[0] & (INST[17:12] == 6'o05);
+    wire op_in    = state[0] & (INST[17:12] == 6'o06);
+    wire op_out   = state[0] & (INST[17:12] == 6'o07);
+    wire op_sxx   = state[0] & (INST[17:15] == 3'o1);
+    wire op_jxx   = state[0] & (INST[17:15] == 3'o2);
+    wire op_alub  = state[0] & (INST[17:15] == 3'o3);
 
-    wire op_alu   = INST[17] && INST[15:12] != 0;
-    wire op_cmp   = INST[17] && INST[15:12] == 0;
-    wire op_alui  = INST[17:16] == 2'o3;
+    wire op_alu   = state[0] & (INST[17] && INST[15:12] != 0);
+    wire op_cmp   = state[0] & (INST[17] && INST[15:12] == 0);
+    wire op_alui  = state[0] & (INST[17:16] == 2'o3);
 
-    assign TIMER_LD = state[0] && op_timer;   // TIMER
+    assign TIMER_LD = !interrupt & state[0] & op_timer;   // TIMER
 
 //-------------------------------------------------------------------
     always @ (posedge CLK) begin
         RUNS <= RUN;
 
-        if (!RUN || !RUNS)
-            begin
-            PC <= 0;
-            state <= STATE_0;
-            RESET <= 1;
-            end
-        else
-            begin
+        RESET <= !RUNS | (!interrupt & state[0] & op_reset);
 
-            RESET <= state[0] & op_reset;  // RESET
+        PC <= (!RUNS)                          ? 18'd0 :           // Reset
+              (interrupt)                      ? {14'd0, VECTOR} : // Interrupt
+              (state[9])                       ? PC :              // Interrupt
+              (state[7] & BRANCH)              ? INST[11:0] :      // JXX
+              (op_jxx)                         ? PC :              // JXX
+              (state[2] | state[3] | state[4]) ? PC :              // LDR, LDC, STOP
+              (op_call)                        ? INST[11:0] :      // CALL
+              (op_pause & !TIMER_ZERO)         ? PC :              // PAUSE
+              (state[6])                       ? DATAIN :          // RTS, RTI 
+                                                 pc_plus_one;      // Default
 
-            PC <= (interrupt)                        ? {14'd0, VECTOR} : // Interrupt
-                  (state[9])                         ? PC :              // Interrupt
-                  (state[7] && BRANCH)               ? INST[11:0] :      // JXX
-                  (state[0] && op_jxx)               ? PC :              // JXX
-                  (state[2] || state[3] || state[4]) ? PC :              // LDR, LDC, STOP
-                  (state[0] && op_call)              ? INST[11:0] :      // CALL
-                  (op_pause && !TIMER_ZERO)          ? PC :              // PAUSE
-                  (state[6])                         ? DATAIN :          // RTS, RTI 
-                                                       pc_plus_one;      // Default
+        PCO <= (interrupt)  ? PC :          // Interrupt
+               (op_call)    ? pc_plus_one : // CALL
+                              PCO;          // Default
 
-            PCO <= (interrupt)             ? PC :          // Interrupt
-                   (state[0] && op_call)   ? pc_plus_one : // CALL
-                                             PCO;          // Default
+        SP <= (!RUNS)                  ? 18'd0      : // Reset
+              (interrupt)              ? SP         : // Interrupt
+              (state[9])               ? SP - 18'd1 : // Interrupt
+              (op_call)                ? SP - 18'd1 : // CALL
+              (state[6])               ? SP + 18'd1 : // RTS, RTI
+              (state[0] & PC == 12'd0) ? 18'd0      : // Restart
+                                         SP;          // Default
 
-            SP <= (state[9])                ? SP - 18'd1 : // Interrupt
-                  (state[0] && op_call)     ? SP - 18'd1 : // CALL
-                  (state[6])                ? SP + 18'd1 : // RTS, RTI
-                  (state[0] && PC == 12'd0) ? 18'd0      : // Restart
-                                              SP;          // Default
+        state <= (!RUNS)     ? STATE_0 : // Reset
+                 (interrupt) ? STATE_9 : // Interrupt
+                 (op_ldi)    ? STATE_1 : // LDI
+                 (op_ldr)    ? STATE_2 : // LDR 
+                 (op_ldc)    ? STATE_3 : // LDC
+                 (op_halt)   ? STATE_4 : // HALt
+                 (state[4])  ? STATE_4 : // HALT
+                 (op_rts)    ? STATE_5 : // RTS
+                 (op_rti)    ? STATE_5 : // RTI
+                 (state[5])  ? STATE_6 : // RTS, RTI
+                 (op_jxx)    ? STATE_7 : // JXX
+                               STATE_0;  // Default
 
-            state <= (interrupt)           ? STATE_9 : // Interrupt
-                     (state[0] && op_ldi)  ? STATE_1 : // LDI
-                     (state[0] && op_ldr)  ? STATE_2 : // LDR 
-                     (state[0] && op_ldc)  ? STATE_3 : // LDC
-                     (state[0] && op_halt) ? STATE_4 : // HALt
-                     (state[4])            ? STATE_4 : // HALT
-                     (state[0] && op_rts)  ? STATE_5 : // RTS
-                     (state[0] && op_rti)  ? STATE_5 : // RTI
-                     (state[5])            ? STATE_6 : // RTS, RTI
-                     (state[0] && op_jxx)  ? STATE_7 : // JXX
-                                             STATE_0;  // Default
+        DATA_SL  <= state[9] |   // Interrupt
+                    (op_call);   // CALL
 
-            DATA_SL  <= state[9] ||           // Interrupt
-                       (state[0] && op_call); // CALL
+        RAM_WR <= (!RUNS)     ? ZERO : // Reset
+                  (interrupt) ? ZERO : // Interrupt
+                  (state[9])  ? ONE  : // Interrupt
+                  (op_call)   ? ONE  : // CALL
+                  (op_str)    ? ONE  : // STR
+                                ZERO;  // Default
 
-            level <= (interrupt)               ? VECTOR :        // Interrupt
-                     (state[0] && op_level)    ? INST[3:0] :     // LEVEL
-                     (state[6] && stat_flag)   ? DATAIN[15:12] : // RTI
-                     (state[0] && PC == 12'd0) ? 4'd15 :         // RESTART
-                                                 level;          // Default 
+        CONST_RD <= state[3]; // LDC
 
-            RAM_WR   <= (state[9])            || // Interrupt
-                        (state[0] && op_call) || // CALL
-                        (state[0] && op_str);    // STR
+        PORT_RD  <= RUNS & !interrupt & op_in;   // IN
 
-            CONST_RD <=  state[3]; // LDC
+        PORT_WR  <= RUNS & !interrupt & op_out;  // OUT
 
-            PORT_RD  <=  state[0] && op_in;   // IN
+        ADRS_SL <= (op_str)   ? 3'd2 : // STR
+                   (op_in)    ? 3'd3 : // IN
+                   (op_out)   ? 3'd3 : // OUT
+                   (state[3]) ? 3'd0 : // LDC
+                   (state[2]) ? 3'd0 : // LDR
+                                3'd1;  // Int, RTS, RTI, CALL
 
-            PORT_WR  <=  state[0] && op_out;  // OUT
+        BIT_LD <= (!RUNS | interrupt)          ? 2'd0 : // Reset Interrupt
+                  (op_alub & INST[14:12] != 0) ? 2'd1 : // ALUB
+                  (op_sxx)                     ? 2'd2 : // SXX
+                                                 2'd0 ; // Default
 
-            ADRS_SL <= (state[0] && op_str) ? 3'd2 : // STR
-                       (state[0] && op_in)  ? 3'd3 : // IN
-                       (state[0] && op_out) ? 3'd3 : // OUT
-                       (state[0] && op_ldc) ? 3'd0 : // LDC
-                       (state[0] && op_ldr) ? 3'd0 : // LDR
-                                              3'd1;  // Int, RTS, RTI, CALL
+        dadrs_temp <= INST[5:0];
+        DADRS <= (state[1] | state[2] | state[3]) ? dadrs_temp : // LDI, LDR, LDC
+                                                    INST[5:0];   // ALU
 
-            BIT_LD <= (state[0] && op_alub && INST[14:12] != 0) ? 2'd1 : // ALUB
-                      (state[0] && op_sxx)                      ? 2'd2 : // SXX
-                                                                  2'd0 ; // Default
+        SADRS <= INST[11:6];
 
-            dadrs_temp <= INST[5:0];
-            DADRS <= (state[1] || state[2] || state[3]) ? dadrs_temp : // LDI, LDR, LDC
-                                                          INST[5:0];   // ALU
-            SADRS <= INST[11:6];
+        REG_WE <= (!RUNS)     ? ZERO : // Reset
+                  (interrupt) ? ZERO : // Interrupt
+                  (op_ldi)    ? ONE  : // LDI
+                  (state[2])  ? ONE  : // LDR
+                  (state[3])  ? ONE  : // LDC
+                  (op_in)     ? ONE  : // IN
+                  (op_alu)    ? ONE  : // ALU, ALUI
+                                ZERO;
 
-            REG_WE <= (state[0] && op_ldi) || // LDI
-                      (state[2])           || // LDR
-                      (state[3])           || // LDC
-                      (state[0] && op_in)  || // IN
-                      (state[0] && op_alu);   // ALU, ALUI
+        ALUS_SL <= (op_ldi)   ? 2'd2 : // LDI
+                   (state[2]) ? 2'd1 : // LDR
+                   (state[3]) ? 2'd1 : // LDC
+                   (op_in)    ? 2'd1 : // IN
+                   (op_alui)  ? 2'd3 : // ALUI
+                                2'd0;  // ALU
 
-            ALUS_SL <= (state[0] && op_ldi)   ? 2'd2 : // LDI
-                       (state[2] || state[3]) ? 2'd1 : // LDR, LDC
-                       (state[0] && op_in)    ? 2'd1 : // IN
-                       (state[0] && op_alui)  ? 2'd3 : // ALUI
-                                                2'd0;  // ALU
+        ALU_OP <= (state[2]) ? 4'd1 :       // LDR
+                  (state[3]) ? 4'd1 :       // LDC
+                  (op_ldi)   ? 4'd1 :       // LDI
+                  (op_in)    ? 4'd1 :       // IN
+                               INST[15:12]; // ALU, ALUB
 
-            ALU_OP <= (state[2] || state[3]) ? 4'd1 :       // LDR, LDC
-                      (state[0] && op_ldi)   ? 4'd1 :       // LDI
-                      (state[0] && op_in)    ? 4'd1 :       // IN
-                                               INST[15:12]; // ALU, ALUB
+        if (state[0]) stat_flag <= op_rti;
 
-            if (state[0]) stat_flag <= op_rti;
+        level <= (!RUNS)                  ? 4'd15 :         // Reset
+                 (interrupt)              ? VECTOR :        // Interrupt
+                 (op_level)               ? INST[3:0] :     // LEVEL
+                 (state[6] & stat_flag)   ? DATAIN[15:12] : // RTI
+                 (state[0] & PC == 12'd0) ? 4'd15 :         // RESTART
+                                            level;          // Default 
 
-            STAT_LD <= (state[5] && stat_flag) ? 2'd3 : // RTI
-                       (state[0] && op_cmp)    ? 2'd1 : // CMP, CMPI
-                       (state[0] && op_alub)   ? 2'd2 : // ALUB
-                                                 2'd0;  // Default
+        STAT_LD <= (!RUNS | interrupt)    ? 2'd0 : // Reset, Interrupt
+                   (state[5] & stat_flag) ? 2'd3 : // RTI
+                   (op_cmp | op_in)       ? 2'd1 : // CMP, CMPI
+                   (op_alu | op_alui)     ? 2'd1 : // CMP, CMPI
+                   (op_ldr | op_ldc)      ? 2'd1 : // CMP, CMPI
+                   (op_alub)              ? 2'd2 : // ALUB
+                                            2'd0;  // Default
 
-            NDATA <= (interrupt)           ? {8'd0, level} :      // Interrupt
-                     (state[0] && op_alui) ? {6'd0, INST[11:6]} : // ALUI
-                     (state[0] && op_in)   ? {6'd0, INST[11:6]} : // IN
-                     (state[0] && op_out)  ? {6'd0, INST[5:0]} :  // OUT
-                                             NDATA;               // Default
+        NDATA <= (interrupt) ? {8'd0, level} :      // Interrupt
+                 (op_alui)   ? {6'd0, INST[11:6]} : // ALUI
+                 (op_in)     ? {6'd0, INST[11:6]} : // IN
+                 (op_out)    ? {6'd0, INST[5:0]} :  // OUT
+                               NDATA;               // Default
 
-            BRANCH_OP <= INST[14:12];
+        BRANCH_OP <= INST[14:12];
 
-            end
     end
 
 endmodule
